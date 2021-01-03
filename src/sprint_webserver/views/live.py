a276407481f83cf50cc836498f6ba0c83ca74bf8
Stig B. Dørmænen <stigbd@gmail.com>
2021-01-03 15:13:23 +0100

    fix: redusert kompleksitet i metode

--- src/sprint_webserver/views/live.py
+++ src/sprint_webserver/views/live.py
@@ -56,25 +56,9 @@ class Live(web.View):
 
         # format time
         for heat in kjoreplan:
-            """Format time from decimal to readable format hh:mm:ss"""
+            # Format time from decimal to readable format hh:mm:ss:
             time = heat["Start"].replace(",", ".")
-            sekunder = int(round(float(time) * 24 * 60 * 60, 0))
-            min = divmod(sekunder, 60)
-            hour = divmod(min[0], 60)
-            if hour[0] < 10:
-                ut_hour = "0" + str(hour[0])
-            else:
-                ut_hour = str(hour[0])
-            if hour[1] < 10:
-                ut_min = "0" + str(hour[1])
-            else:
-                ut_min = str(hour[1])
-            if min[1] < 10:
-                ut_sek = "0" + str(min[1])
-            else:
-                ut_sek = str(min[1])
-            logging.debug("Tid: " + ut_hour + ":" + ut_min + ":" + ut_sek)
-            heat["Start"] = ut_hour + ":" + ut_min + ":" + ut_sek
+            heat["Start"] = _format_time(time)
 
         """Get route function."""
         return await aiohttp_jinja2.render_template_async(
@@ -89,3 +73,25 @@ class Live(web.View):
                 "startliste": startliste,
             },
         )
+
+
+def _format_time(decimal_time: str) -> str:
+    """Format time from decimal to readable format hh:mm:ss."""
+    sekunder = int(round(float(decimal_time) * 24 * 60 * 60, 0))
+    min = divmod(sekunder, 60)
+    hour = divmod(min[0], 60)
+    if hour[0] < 10:
+        ut_hour = "0" + str(hour[0])
+    else:
+        ut_hour = str(hour[0])
+    if hour[1] < 10:
+        ut_min = "0" + str(hour[1])
+    else:
+        ut_min = str(hour[1])
+    if min[1] < 10:
+        ut_sek = "0" + str(min[1])
+    else:
+        ut_sek = str(min[1])
+    logging.debug("Tid: " + ut_hour + ":" + ut_min + ":" + ut_sek)
+
+    return ut_hour + ":" + ut_min + ":" + ut_sek
