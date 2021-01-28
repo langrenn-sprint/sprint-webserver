1717d58a1f7c520b269655057108c30dc426a6f5
Stig B. Dørmænen <stigbd@gmail.com>
2021-01-28 09:20:36 +0100

    fix: fix type

--- src/sprint_webserver/views/start.py
+++ src/sprint_webserver/views/start.py
@@ -19,7 +19,7 @@ class Start(web.View):
         informasjon = ""
         startliste = []
         kjoreplan = []
-        klassetider = ""
+        klassetider = {}
         colseparators = []
         colclass = "w3-third"
 
